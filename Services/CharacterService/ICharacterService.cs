320a7b5252698a110328daaf3f2f99d266db1df9
Benasi000 <bendzo13@gmail.com>
2022-11-22 19:34:52 +0100

    Asynchronous calls

--- Services/CharacterService/ICharacterService.cs
+++ Services/CharacterService/ICharacterService.cs
@@ -2,11 +2,11 @@ namespace dotnet6_rpg.Services.CharacterService
 {
     public interface ICharacterService
     {
-        List<Character> GetAllCharacters();
+        Task<List<Character>> GetAllCharacters();
 
-        Character GetCharacterById(int id);
+       Task<Character> GetCharacterById(int id);
 
-        List<Character> AddCharacter(Character newCharacter);
+        Task<List<Character>> AddCharacter(Character newCharacter);
 
     }
 }
